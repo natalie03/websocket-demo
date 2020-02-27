d17e5a32fa80dc1d1076d36e4ce1eeeb93ca0017
Natalie Miller <nataliemiller03@gmail.com>
2020-02-26 18:15:21 -0800

    Little socket.io demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import socketIOClient from "socket.io-client";
 
-function App() {
+const App = () => {
+  const [response, setResponse] = useState(null);
+
+  const endpoint = "wss://nu-gong-server.glitch.me";
+  const socket = socketIOClient(endpoint);
+
+  socket.on("partyAlert", data => {
+    console.log(data);
+    setResponse(data);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        border: '30px solid #521945',
+        backgroundColor: '#EAF2EF',
+        color: '#912F56',
+        fontSize: '30px',
+        fontWeight: 'bold',
+        boxSizing: 'border-box',
+        letterSpacing: '1px'
+       }}>
+        {response
+            ? <p>
+              {`Hooray! ${response.team} launched ${response.project}`}
+            </p>
+            : <p>Waiting...</p>}
+      </div>
   );
 }
-
 export default App;
